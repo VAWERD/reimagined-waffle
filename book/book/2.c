4616b1503020914bcd4217686b60f7519cd463d9
VAWERD <137434948+VAWERD@users.noreply.github.com>
2023-06-29 22:46:41 +0800

    Add files via upload

---
+++ book/book/2.c
@@ -0,0 +1,116 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>;
+//int main(void)
+//{
+//	int num;
+//	num = 1;
+//
+//	printf("I am a simple");
+//	printf("computer.\n");
+//	printf("My favorite number is %d because it is first.\n", num);
+//	
+//	return 0;
+//}
+
+
+
+//int main(void)
+//{
+//	int fathoms = 2;
+//	int feet=6*fathoms;
+//
+//	printf("%d音寻等于%d英尺。\n", fathoms, feet);
+//	printf("是的，我说的是%d英尺！",6*fathoms);
+//	return 0;
+//}
+//{
+//	int feet, fathoms;
+//
+//	scanf("%d", &fathoms);
+//	feet = fathoms * 6;
+//	printf("%音寻等于%d英尺", fathoms, feet);
+//
+//	return 0;
+//}
+
+
+//			2.12
+//int main(void)
+//{
+//	printf("Gustav Manler\n");
+//	printf("Gustav \nManler \n");
+//	printf("Gustav ");
+//	printf("Manler\n");
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	int nianling = 0;
+//	scanf("%d", &nianling);
+//	printf("在%d岁时，你过了%d天。", nianling, 365 * nianling);
+//	return 0;
+//}
+
+//int jolly(void)
+//{
+//	printf("For he, s a jolly good fellow!\n");
+//}
+//int deny(void)
+//{
+//	printf("Whicn nobody can deny!\n");
+//}
+//int main(void)
+//{
+//	jolly();
+//	jolly();
+//	jolly();
+//	deny();
+//	return 0;
+//}
+
+//int br(void)
+//{
+//	printf("Brazil,Russia");
+//}
+//int ic(void)
+//{
+//	printf("India,China");
+//}
+//int main(void)
+//{
+//	br(); printf(","); ic(); printf("\n");
+//
+//	ic(); printf(",\n");
+//	
+//	br();
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	int toes = 10;
+//	printf("toes=%d\ntoes的二倍=%d\ntoes的平方=%d",toes, 2 * toes, toes*toes );
+//	return 0;
+//}
+
+//void one_three(void);
+//void two(void);
+//int main(void)
+//{
+//	printf("starting now:\n");
+//	one_three();
+//	printf("done!");
+//	return 0;
+//}
+//void one_three(void)
+//{
+//	printf("one\n");
+//	two();
+//	printf("three\n");
+//}
+//void two(void)
+//{
+//	printf("two\n");
+//}
